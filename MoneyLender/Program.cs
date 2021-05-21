7c53870de4bc7aec7447f7f49b0f89b2cd40a4e3
Tunji Oyewole <t.oyewole23@outlook.com>
2021-05-21 17:34:46 -0400

    Add project files.

---
+++ MoneyLender/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace MoneyLender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Guy joe = new Guy() { Name = "Joe", Cash = 50 };
+            Guy bob = new Guy() { Name = "Bob", Cash = 100 };
+
+            while (true)
+            {
+                joe.WriteMyInfo();
+                bob.WriteMyInfo();
+
+                Console.Write("Enter an amount: ");
+                string howMuch = Console.ReadLine();
+                if (howMuch == "") return;
+                if (int.TryParse(howMuch, out int result))
+                {
+                    Console.Write("Who should give the cash: ");
+                    string whichGuy = Console.ReadLine();
+                    if (whichGuy == "Joe")
+                    {
+                        int amount = joe.GiveCash(result);
+                        bob.ReceiveCash(amount);
+                    }
+                    else if (whichGuy == "Bob")
+                    {
+                        int amount = bob.GiveCash(result);
+                        joe.ReceiveCash(amount);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter 'Joe' or 'Bob'");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please enter an amount (or a blank line to exit).");
+                }
+            }
+        }
+    }
+}
